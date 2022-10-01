488055cd618fb1579a73ca87a21e3d07988e311a
Pradit Yamsook <pradit.y@ku.th>
2022-10-01 18:57:12 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import React, { useEffect } from "react";
+
+// function RGBtoHex(r, g, b) {
+//   const rHex = r.toString(16);
+//   const gHex = g.toString(16);
+//   const bHex = b.toString(16);
+//   return `#${rHex}${gHex}${bHex}`;
+// }
+
+function loopComponent() {
+    let componentArr = [];
+    for (let index = 0; index < 9; index++) {
+        componentArr.push(
+            <div className="child">
+                <canvas id={`rect${index}`} width="100px" height="100px">
+                    {index}
+                </canvas>
+            </div>
+        );
+    }
+    return componentArr;
+}
+
+function App() {
+    useEffect(() => {
+        for (let index = 0; index < 9; index++) {
+            let element = document.getElementById(`rect${index}`);
+            let ctx = element.getContext("2d");
+            ctx.beginPath();
+            ctx.fillStyle = `rgb(
+              ${Math.floor(Math.random() * (255 - 0))},
+              ${Math.floor(Math.random() * (255 - 0))},
+              ${Math.floor(Math.random() * (255 - 0))}
+            )`;
+            ctx.fillRect(0, 0, 100, 100);
+            ctx.stroke();
+        }
+    });
+    return <div className="continaer">{loopComponent()}</div>;
+}
+
+export default App;
